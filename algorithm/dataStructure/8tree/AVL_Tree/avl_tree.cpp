8940a282e71f14f710d98655c93472d4d94a9337
huangcy <huangcy@autoai.com>
2022-09-07 19:20:46 +0800

    update avl_tree.cpp

--- algorithm/dataStructure/8tree/AVL_Tree/avl_tree.cpp
+++ algorithm/dataStructure/8tree/AVL_Tree/avl_tree.cpp
@@ -204,6 +204,89 @@ AvlTreeNode* insertAvlTreeNode_recursive(AvlTreeNode* root, int newVal)
     return root;
 }
 
+AvlTreeNode* getMinNodeOfRightTree(AvlTreeNode* root)
+{
+    if (!root)
+        return root;
+    
+    while (root->left)
+    {
+        root = root->left;
+    }
+    return root;
+}
+
+/*
+    20            20   BF(root->left)>=0     20  BF(root->left)<0   20       20 BF(root->right)<=0  20     BF(root->right)>0
+   /   \        /    \   BF(10)>=0          /  \    BF(10)<0       /  \     /  \     BF(30)<=0     /  \     BF(30)>0
+  10    30     10    30 删除30, LL右旋     10   30  删除30, LR     10  30   10  30   删除10, RR    10   30  删除10, RL
+  /           /  \                          \                           \      / \                    /  
+ 8           8   15                         15                          40    25 40                  25  
+
+*/
+AvlTreeNode* deleteNodeOfAvlTree_revursive(AvlTreeNode* root, int key)
+{
+    if(!root)
+        return root;
+    
+    if(key < root->data)
+    {
+        root->left = deleteNodeOfAvlTree_revursive(root->left, key);
+    }
+    else if(key > root->data)
+    {
+        root->right = deleteNodeOfAvlTree_revursive(root->right, key);
+    }
+    else
+    {
+        if (root->left == NULL) // 只有一个节点或者无节点
+        {
+            AvlTreeNode* rightNode = root->right;
+            return rightNode;
+        }
+        else if(root->right == NULL)    // 只有一个节点或者无节点
+        {
+            AvlTreeNode* leftNode = root->left;
+            return leftNode;
+        }
+        else   // 有两个节点
+        {
+            AvlTreeNode* minNode = getMinNodeOfRightTree(root->right);
+            root->data = minNode->data;
+            root->right = deleteNodeOfAvlTree_revursive(root->right, key);
+        }
+    }
+
+    root->height = avlTreeHeight(root); // 更新当前节点的树的高度
+    int balanceFactor = getBalanceFactor(root);
+
+    if(balanceFactor == 2)  // 左子树比右子树高
+    {
+        if (getBalanceFactor(root->left) >= 0 )   // root的左子树的的根节点的左子树插入元素导致失衡
+        {
+            return rightRotation(root);
+        }
+        else if (getBalanceFactor(root->left) < 0)
+        {
+            return leftRightRotation(root);
+        }
+    }
+    else if (balanceFactor == -2)   // 右子树比左子树高
+    {
+         if (getBalanceFactor(root->right) > 0 )   // root的左子树的的根节点的左子树插入元素导致失衡
+        {
+            return rightLeftRotation(root);
+        }
+        else if (getBalanceFactor(root->left) <= 0)
+        {
+            return leftRotation(root);
+        }
+    }
+    
+    
+    return root;
+}
+
 void preOrderTraverseAvl(AvlTreeNode *root)
 {
     if (root)
@@ -234,6 +317,85 @@ void postOrderTraverseAvl(AvlTreeNode *root)
         printf("%d ", root->data);
     }
 }
+
+void deleteAvlNodeLL()
+{
+    AvlTreeNode* root = NULL;
+    root = insertAvlTreeNode_recursive(root, 10);
+    root = insertAvlTreeNode_recursive(root, 20);
+    root = insertAvlTreeNode_recursive(root, 30);
+    root = insertAvlTreeNode_recursive(root, 8);
+    preOrderTraverseAvl(root);  // 20 10 8 30
+    putchar(10);
+    inOrderTraverseAvl(root);   // 8 10 20 30
+    putchar(10);
+
+    root = deleteNodeOfAvlTree_revursive(root, 30);
+    preOrderTraverseAvl(root);  // 10 8 20
+    putchar(10);
+    inOrderTraverseAvl(root);   // 8 10 20
+    putchar(10);
+}
+
+void deleteAvlNodeLR()
+{
+    AvlTreeNode* root = NULL;
+    root = insertAvlTreeNode_recursive(root, 20);
+    root = insertAvlTreeNode_recursive(root, 10);
+    root = insertAvlTreeNode_recursive(root, 8);
+    root = insertAvlTreeNode_recursive(root, 30);
+    root = insertAvlTreeNode_recursive(root, 15);
+    preOrderTraverseAvl(root);  // 20 10 8 15 30
+    putchar(10);
+    inOrderTraverseAvl(root);   // 8 10 15 20 30
+    putchar(10);
+
+    root = deleteNodeOfAvlTree_revursive(root, 30);
+    preOrderTraverseAvl(root);  // 10 8 20 15
+    putchar(10);
+    inOrderTraverseAvl(root);   // 8 10 15 20
+    putchar(10);
+}
+
+void deleteAvlNodeRR()
+{
+    AvlTreeNode* root = NULL;
+    root = insertAvlTreeNode_recursive(root, 20);
+    root = insertAvlTreeNode_recursive(root, 10);
+    root = insertAvlTreeNode_recursive(root, 30);
+    root = insertAvlTreeNode_recursive(root, 25);
+    root = insertAvlTreeNode_recursive(root, 40);
+    preOrderTraverseAvl(root);  // 20 10 30 25 40
+    putchar(10);
+    inOrderTraverseAvl(root);   // 10 20 25 30 40
+    putchar(10);
+
+    root = deleteNodeOfAvlTree_revursive(root, 10);
+    preOrderTraverseAvl(root);  // 30 20 25 40
+    putchar(10);
+    inOrderTraverseAvl(root);   // 20 25 30 40
+    putchar(10);
+}
+
+void deleteAvlNodeRL()
+{
+    AvlTreeNode* root = NULL;
+    root = insertAvlTreeNode_recursive(root, 20);
+    root = insertAvlTreeNode_recursive(root, 10);
+    root = insertAvlTreeNode_recursive(root, 30);
+    root = insertAvlTreeNode_recursive(root, 25);
+    preOrderTraverseAvl(root);  // 20 10 30 25
+    putchar(10);
+    inOrderTraverseAvl(root);   // 10 20 25 30
+    putchar(10);
+
+    root = deleteNodeOfAvlTree_revursive(root, 10);
+    preOrderTraverseAvl(root);  // 25 20 30
+    putchar(10);
+    inOrderTraverseAvl(root);   // 20 25 30
+    putchar(10);
+}
+
 
 /*
     1.root就是最小不平衡子树的根节点  2.注意是右子树不是右节点
@@ -269,12 +431,24 @@ int main()
 
     preOrderTraverseAvl(root);  // 10 8 6 9 40 30 20 35 50 60
     putchar(10);
-
     inOrderTraverseAvl(root);   // 6 8 9 10 20 30 35 40 50 60
     putchar(10);
-
     postOrderTraverseAvl(root); // 6 9 8 20 35 30 60 50 40 10
     putchar(10);
+
+    root = deleteNodeOfAvlTree_revursive(root, 20);
+    root = deleteNodeOfAvlTree_revursive(root, 35);
+    root = deleteNodeOfAvlTree_revursive(root, 30);
+
+    preOrderTraverseAvl(root);  // 10 8 6 9 50 40 60
+    putchar(10);
+    inOrderTraverseAvl(root);   // 6 8 9 10 40 50 60
+    putchar(10);
+
+    // deleteAvlNodeLL();
+    // deleteAvlNodeLR();
+    // deleteAvlNodeRR();
+    deleteAvlNodeRL();
     
     return 0;
 }
