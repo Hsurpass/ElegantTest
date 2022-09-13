e55ece1acaaf38e4ed821abda7792af8b4812227
huangcy <huangcy@autoai.com>
2022-09-13 19:19:09 +0800

    update astar.cpp

--- algorithm/aStar/astar.cpp
+++ algorithm/aStar/astar.cpp
@@ -46,6 +46,7 @@ public:
 
     void reset() { f = g = h = -1; parent = NULL; }
     bool operator<(const Node& other) const { return this->f < other.f; }
+    bool operator>(const Node& other) const { return this->f > other.f; }
     // bool operator==(const Node& other) const { return this->p.x == other.p.x && this->p.y == other.p.y; }
 
 };
@@ -60,7 +61,7 @@ public:
     void printPath();
 
     vector<vector<Node *>> aStarMap;
-    std::priority_queue<Node/*, vector<Node>, std::less<Node>*/ > openList;
+    std::priority_queue<Node, vector<Node>, std::greater<Node> > openList;  // 小顶堆
     vector<Node*> path;
 };
 
@@ -82,39 +83,47 @@ void Astar::initMap()
     aStarMap.resize(10);
     for (int i = 0; i < 10; i++)
     {
-        vector<Node*> map = aStarMap[i];
+        vector<Node*>& map = aStarMap[i];
         for (int j = 0; j < 10; j++)
         {
             Node *node = new Node();
             node->p.x = i;
-            node->p.x = j;
+            node->p.y = j;
             node->type = NodeType_none;
 
             map.push_back(node);
         }
     }
 
-    // Node*& node = aStarMap[2][3];
-    // node->type = NodeType_wall;
-    // aStarMap[2][3]->type = NodeType_wall;
-    // aStarMap[3][3]->type = NodeType_wall;
-    // aStarMap[4][3]->type = NodeType_wall; 
-    // aStarMap[5][3]->type = NodeType_wall;
+    Node*& node = aStarMap[2][3];
+    node->type = NodeType_wall;
+    aStarMap[2][3]->type = NodeType_wall;
+    aStarMap[3][3]->type = NodeType_wall;
+    aStarMap[4][3]->type = NodeType_wall; 
+    aStarMap[5][3]->type = NodeType_wall;
+    aStarMap[5][4]->type = NodeType_wall;
+    aStarMap[2][5]->type = NodeType_wall;
+    aStarMap[3][5]->type = NodeType_wall;
+    aStarMap[4][5]->type = NodeType_wall; 
+    aStarMap[5][5]->type = NodeType_wall;
+
+    aStarMap[2][4]->type = NodeType_wall;
+
 }
 
 void Astar::getOneNeighborNode(int x, int y, int g, Node* curr, Node* end)
 {
-    if (x < 0 || y < 0)
+    if ((x < 0 || x >= 10) || (y < 0 || y >= 10) )
         return ;
 
     Node* neighborNode = aStarMap[x][y];
-    if (neighborNode->type == NodeType_wall || neighborNode->type == NodeType_close || neighborNode->type == NodeType_open)
+    if (neighborNode == NULL || neighborNode->type == NodeType_wall || neighborNode->type == NodeType_close || neighborNode->type == NodeType_open)
     {
         return ;
     }
     
     neighborNode->h = abs(end->p.x - x) + abs(end->p.y - y);
-    neighborNode->g = neighborNode->parent->g + g;
+    neighborNode->g = curr->g + g;
     neighborNode->f = neighborNode->g + neighborNode->h;
     neighborNode->type = NodeType_open;
     neighborNode->parent = curr;
@@ -129,10 +138,11 @@ bool Astar::findPath(Point start, Point end)
         cout << "start or end fail!!!" << endl;
         return false;
     }
+
     Node* startNode = aStarMap[start.x][start.y];
     startNode->g = 0;
     Node* endNode = aStarMap[end.x][end.y];
-    if(startNode->type == NodeType_wall || startNode->type == NodeType_wall)
+    if(startNode->type == NodeType_wall || endNode->type == NodeType_wall)
     {
         cout << "not arrive" << endl;
         return false;
@@ -144,8 +154,8 @@ bool Astar::findPath(Point start, Point end)
     {
         Node currNode = openList.top();
         openList.pop();
-        const Node*&aStarMap[currNode.p.x][currNode.p.y];
-        aStarMap[currNode.p.x][currNode.p.y]->type = NodeType_close;
+        Node* realNode = aStarMap[currNode.p.x][currNode.p.y];
+        realNode->type = NodeType_close;
 
         if (currNode.p.x == end.x && currNode.p.y == end.y) // 找到终点了
         {
@@ -162,28 +172,28 @@ bool Astar::findPath(Point start, Point end)
         }
 
         // 上 x-1, y
-        getOneNeighborNode(currNode.p.x - 1, currNode.p.y, 10, endNode);        
+        getOneNeighborNode(currNode.p.x - 1, currNode.p.y, 10, realNode, endNode);        
         
         // 下 x+1, y
-        getOneNeighborNode(currNode.p.x + 1, currNode.p.y, 10, endNode);
+        getOneNeighborNode(currNode.p.x + 1, currNode.p.y, 10, realNode, endNode);
 
         // 左 x, y-1
-        getOneNeighborNode(currNode.p.x, currNode.p.y - 1, 10, endNode);
+        getOneNeighborNode(currNode.p.x, currNode.p.y - 1, 10, realNode, endNode);
 
         // 右 x, y+1
-        getOneNeighborNode(currNode.p.x, currNode.p.y + 1, 10, endNode);
+        getOneNeighborNode(currNode.p.x, currNode.p.y + 1, 10, realNode, endNode);
 
         // 左上
-        getOneNeighborNode(currNode.p.x - 1, currNode.p.y - 1, 14, endNode);
+        getOneNeighborNode(currNode.p.x - 1, currNode.p.y - 1, 14, realNode, endNode);
 
         // 右上
-        getOneNeighborNode(currNode.p.x - 1, currNode.p.y + 1, 14, endNode);
+        getOneNeighborNode(currNode.p.x - 1, currNode.p.y + 1, 14, realNode, endNode);
 
         // 左下
-        getOneNeighborNode(currNode.p.x + 1, currNode.p.y - 1, 14, endNode);
+        getOneNeighborNode(currNode.p.x + 1, currNode.p.y - 1, 14, realNode, endNode);
 
         // 右下
-        getOneNeighborNode(currNode.p.x + 1, currNode.p.y + 1, 14, endNode);
+        getOneNeighborNode(currNode.p.x + 1, currNode.p.y + 1, 14, realNode, endNode);
         
     }
     
@@ -196,9 +206,9 @@ void Astar::printPath()
 {
     for (int i = 0; i < path.size(); i++)
     {
-        cout << "(" << path[i]->p.x << ", " << path[i]->p.y << ")" << endl;
+        cout << "(" << path[i]->p.x << ", " << path[i]->p.y << ")";
     }
-    
+    cout << endl;
 }
 
 int main()
@@ -206,7 +216,11 @@ int main()
     Astar astar;
     astar.initMap();
     Point start{3, 1};
-    Point end{3, 6};
+    // Point end{3, 6};
+    // Point end{3, 4};
+    Point end{9, 9};
+
+
     astar.findPath(start, end);
     astar.printPath();
 
