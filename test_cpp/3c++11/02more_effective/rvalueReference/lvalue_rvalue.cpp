85c223f8e34ccd48e2e5650594469f267497f4e4
bruce <1742143252@qq.com>
2023-01-10 22:01:21 +0800

    update lvalue_rvalue.cpp

--- test_cpp/3c++11/02more_effective/rvalueReference/lvalue_rvalue.cpp
+++ test_cpp/3c++11/02more_effective/rvalueReference/lvalue_rvalue.cpp
@@ -13,7 +13,7 @@ public:
     // 若拷贝自实现，移动默认即无
     Copy(const Copy &another) : _i(new int(*another._i))
     {
-        cout << " Copy(const Copy & another)" << endl;
+        cout << "Copy(const Copy & another), copy " << &another << " to " << this << endl;
     }
     // 若移动自实现，拷贝默认即无
     Copy(Copy &&another) : _i(nullptr)
@@ -22,12 +22,13 @@ public:
         _i = another._i;
         another._i = nullptr;
     }
+
     Copy &operator=(Copy &&another)
     {
         if (this != &another)
         {
 
-            cout << "MOVE operator=(Copy&& another)" << endl;
+            cout << "MOVE operator=(Copy&& another), move assign " << &another << " to " << this << endl;
             delete _i;
             _i = another._i;
             another._i = nullptr;
@@ -37,7 +38,7 @@ public:
     }
     Copy &operator=(const Copy &another)
     {
-        cout << "Copy& operator=(const Copy& another)" << endl;
+        cout << "Copy& operator=(const Copy& another), copy assign " << &another << " to " << this << endl;
 
         if (&another != this)
         {
@@ -47,11 +48,13 @@ public:
 
         return *this;
     }
+
     ~Copy()
     {
         cout << "~Copy()" << this << endl;
         delete _i;
     }
+
     void dis() const
     {
         cout << *_i << endl;
@@ -59,6 +62,15 @@ public:
 
     int *_i;
 };
+
+//getObjectNROV函数返回类型仍然是Copy，而不是Copy&&。
+//这意味着函数还是会创建一个Copy类的临时对象，只是临时对象是通过右值引用得到的，也就是说通过移动构造函数移动得到的。
+//如果没定义move constructor，则调用copy constructor，
+Copy getObjectNROV()
+{
+    Copy c = Copy();
+    return std::move(c);    // 只要有std::move就会调一次移动构造,没有移动构造调用拷贝构造
+}
 
 Copy getObject()
 {
@@ -70,8 +82,9 @@ const Copy getConstObject()
     return Copy();
 }
 
+//函数的返回类型是右值引用，return右值引用，没有临时对象的消耗，但是仍不可取，因为右值引用的对象在使用前已经析构了。
 // 将亡值
-Copy&& getXvalueObject()
+Copy &&getXvalueObject()
 {
     return Copy();
 }
@@ -115,9 +128,9 @@ void print_type_and_value(T &&t)
     std::cout << "T: " << std::is_lvalue_reference<T>::value << std::endl;
     std::cout << "T: " << std::is_rvalue_reference<T>::value << std::endl;
 
-    std::cout << "t: " << std::is_reference<T&&>::value << std::endl;
-    std::cout << "t: " << std::is_lvalue_reference<T&&>::value << std::endl;
-    std::cout << "t: " << std::is_rvalue_reference<T&&>::value << std::endl;
+    std::cout << "t: " << std::is_reference<T &&>::value << std::endl;
+    std::cout << "t: " << std::is_lvalue_reference<T &&>::value << std::endl;
+    std::cout << "t: " << std::is_rvalue_reference<T &&>::value << std::endl;
 
     print(std::forward<T>(t));
 }
@@ -130,7 +143,7 @@ void test_referenceCollapsing_and_prefectForward()
     // print_type_and_value<string&>(string& t)
     cout << "-----------lvalue:-----------" << endl;
     Copy s1(1);
-    print_type_and_value(s1);   // print(T &v), lvalue
+    print_type_and_value(s1); // print(T &v), lvalue
 
     // The following call resolves to:
     // print_type_and_value<const string&>(const string& && t)
@@ -138,12 +151,12 @@ void test_referenceCollapsing_and_prefectForward()
     // print_type_and_value<const string&>(const string& t)
     cout << "---------const lvalue-------------" << endl;
     const Copy s2(2);
-    print_type_and_value(s2);   // print(const T &v), lvalue,
+    print_type_and_value(s2); // print(const T &v), lvalue,
 
     // The following call resolves to:
     // print_type_and_value<string&&>(string&& t)
     cout << "---------pure rvalue-------------" << endl;
-    print_type_and_value(Copy(3));  // print(T &&v), rvalue,
+    print_type_and_value(Copy(3)); // print(T &&v), rvalue,
 
     // The following call resolves to:
     // print_type_and_value<const string&&>(const string&& t)
@@ -153,14 +166,13 @@ void test_referenceCollapsing_and_prefectForward()
     // The following call resolves to:
     // print_type_and_value<string&&>(string&& t)
     cout << "----------rvalue------------" << endl;
-    print_type_and_value(getObject());  // print(T &&v), rvalue,
+    print_type_and_value(getObject()); // print(T &&v), rvalue,
 
     // std::move 会把左值转换为右值
     // The following call resolves to:
     // print_type_and_value<string&&>(string&& t)
     cout << "------------std::move----------" << endl;
-    print_type_and_value(std::move(s1) );   // print(T &&v), rvalue
-
+    print_type_and_value(std::move(s1)); // print(T &&v), rvalue
 }
 
 /*********************引用折叠****************************/
@@ -202,17 +214,17 @@ void test_lvalue()
     int a = 5;
     int &a1 = a;
     cout << "&a1: " << &a1 << endl;
-    cout << "&(a = 1)" << &(a=1) << endl;   // lvalue
-    cout << "&(++a)" << &(++a) << endl;   // lvalue
+    cout << "&(a = 1)" << &(a = 1) << endl; // lvalue
+    cout << "&(++a)" << &(++a) << endl;     // lvalue
     // cout << "&(a++)" << &(a++) << endl; // rvalue
-    cout << "&(cout<<'')" << &(cout << ' ') << endl;    // lvalue operator<<
+    cout << "&(cout<<'')" << &(cout << ' ') << endl; // lvalue operator<<
 
     const int &a2 = 5;
-    cout << "&a2: " << &a2 << endl;
+    cout << "&a2: " << &a2 << endl; // lvalue
 
-    vector<int> v = {1,2,3};
-    auto&& v1 = v[0];   // v1 is lvalue
-    cout << &v1 << endl;
+    vector<int> v = {1, 2, 3};
+    auto &&v1 = v[0]; // v1 is lvalue
+    cout << "&v1:" << &v1 << endl;
 }
 
 void test_rvalue()
@@ -222,24 +234,55 @@ void test_rvalue()
     int &&a1 = std::move(a);
     // int& b1 = std::move(b);  // error
 
-    cout << "a1:" << a1 << endl;
-    cout << "a:" << a << endl;
-    cout << "&a1:" << &a1 << endl;
-    cout << "&a:" << &a << endl;
+    cout << "a1:" << a1 << endl;    //10
+    cout << "a:" << a << endl;      //10
+    cout << "&a1:" << &a1 << endl;  //0x7ffc029ea5a0
+    cout << "&a:" << &a << endl;    //0x7ffc029ea5a0
+    a1 = 100;
+    cout << "a1:" << a1 << endl;    //100
+    cout << "a:" << a << endl;      //100
+    cout << "&a1:" << &a1 << endl;  //0x7ffc029ea5a0
+    cout << "&a:" << &a << endl;    //0x7ffc029ea5a0
 
-    Copy c = getObject();
-    c.dis();
-    const Copy &c1 = getObject();
-    c1.dis();
-    Copy &&c2 = getObject();
-    c2.dis();
+    // c++编译器默认是开启RVO的，-fno-elide-constructors关闭RVO
+#if 0
+    {
+        Copy c = getObject();   // 1.关闭RVO, 如果只实现了copy constructor, 先把返回值拷贝给临时对象，再把临时对象拷贝给c
+        c.dis();                // 2.关闭RVO, 如果实现了move constructor, 则先把返回值移动给临时对象，再把临时对象移动给c
+        cout << "&c:" << &c << endl;
+    }
+#endif
+#if 0
+    {
+        // 1.关闭RVO, 对于常量左值引用，如果只实现了copy constructor, 则直接把返回值拷贝给c1
+        // 2.关闭RVO, 对于常量左值引用，如果实现了move constructor, 则直接把返回值移动给c1
+        // const Copy &c1 = getObject();   
+        // const Copy &c1 = getConstObject();   
+        // 3.1关闭RVO, 如果只实现了copy constructor, 先把返回值拷贝给临时对象，再把临时对象拷贝给c1
+        // 3.2关闭RVO，对于右值引用，如果实现了move constructor, 则先把返回值移动给临时变量，再把临时变量移动给c1
+        const Copy &c1 = getObjectNROV();   
+        // const Copy &c1 = getXvalueObject(); // error egmentation fault
+        c1.dis();                       
+        cout << "&c1:" << &c1 << endl;
+    }
+#endif
+    cout << "****************************************" << endl;
+#if 1
+    {
+        // Copy &&c2 = getObject();    // 关闭RVO，对于右值引用，如果实现了move constructor, 则直接把返回值移动给c2
+        Copy &&c2 = getObjectNROV();// 关闭RVO，对于右值引用，如果实现了move constructor, 则先把返回值移动给临时变量，再把临时变量移动给c2
+        // Copy &&c2 = getXvalueObject();  // Segmentation fault
+        c2.dis();
+        cout << "&c2:" << &c2 << endl;
+    }
+#endif
 }
 
 void test_xvalue()
 {
     // getObject().dis();
     // cout << &(getXvalueObject()) << endl;   // xvalue不能&
-    const Copy& c = getXvalueObject();    // xvalue
+    const Copy &c = getXvalueObject(); // xvalue
     c.dis();
 }
 
@@ -247,11 +290,11 @@ int main()
 {
     // test_lvalue();
     cout << "----------------------" << endl;
-    // test_rvalue();
+    test_rvalue();
     cout << "----------------------" << endl;
     // test_xvalue();
     // fromConstTRefToRefref();
-    test_referenceCollapsing_and_prefectForward();
+    // test_referenceCollapsing_and_prefectForward();
 
     return 0;
 }
