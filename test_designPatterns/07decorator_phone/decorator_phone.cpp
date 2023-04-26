aa96810c9bec39110a5b1910499f73d93172259d
hchy <1742143252@qq.com>
2023-04-26 17:44:46 +0800

    update decorator

--- test_designPatterns/07decorator_phone/decorator_phone.cpp
+++ test_designPatterns/07decorator_phone/decorator_phone.cpp
@@ -6,10 +6,10 @@ class Phone
 {
 public:
     virtual int cost() = 0;
-    virtual ~Phone(){}
+    virtual ~Phone() {}
 };
 
-class IPhone:public Phone
+class IPhone : public Phone
 {
 public:
     int cost() { return 8000; }
@@ -20,42 +20,42 @@ public:
 class BaseDecorator : public Phone
 {
 public:
-    BaseDecorator(Phone* phone):m_phone(phone){}
+    BaseDecorator(Phone *phone) : m_phone(phone) {}
     int cost() { cout << "BaseDecorator" << endl; }
+
 protected:
-    Phone* m_phone;
+    Phone *m_phone;
 };
 
 class PhoneScreen : public BaseDecorator
 {
 public:
-    PhoneScreen(Phone* phone):BaseDecorator(phone){}
-    int cost() { return m_phone->cost() + 200;}
+    PhoneScreen(Phone *phone) : BaseDecorator(phone) {}
+    int cost() { return m_phone->cost() + 200; }
 };
 class PhoneShell : public BaseDecorator
 {
 public:
-    PhoneShell(Phone* phone):BaseDecorator(phone){}
-    int cost() { return m_phone->cost() + 20; }   
+    PhoneShell(Phone *phone) : BaseDecorator(phone) {}
+    int cost() { return m_phone->cost() + 20; }
 };
 
 void test_decorator01()
 {
-    Phone* phone = new IPhone();
+    Phone *phone = new IPhone();
     cout << phone->cost() << endl;
 
-    PhoneScreen* ps = new PhoneScreen(phone);
+    PhoneScreen *ps = new PhoneScreen(phone);
     cout << ps->cost() << endl;
-    
-    PhoneShell* pshell = new PhoneShell(phone);
+
+    PhoneShell *pshell = new PhoneShell(phone);
     cout << pshell->cost() << endl;
     delete pshell;
     delete ps;
     delete phone;
 
-    Phone* allphone = new PhoneScreen(new PhoneShell(new IPhone()));
+    Phone *allphone = new PhoneScreen(new PhoneShell(new IPhone()));
     cout << allphone->cost() << endl;
-    
 }
 
 int main()
