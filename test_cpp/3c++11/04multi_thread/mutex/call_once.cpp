7ea12f498cfb3cf16a042b750e7983696950025f
hchy <1742143252@qq.com>
2023-05-28 23:41:37 +0800

    add call_once.cpp

---
+++ test_cpp/3c++11/04multi_thread/mutex/call_once.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+int g_val = -1;
+int count = 0;
+std::once_flag flag; 
+void setVal(int x)
+{   
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    count++;
+    g_val = x;
+}
+
+void test_call_once(int id)
+{
+    cout << "enter test_call_once" << endl;
+    std::call_once(flag, setVal, id);
+    cout << "leave test_call_once" << endl;
+}
+
+int main()
+{
+    thread t[5];
+    for (size_t i = 0; i < 5; i++)
+    {
+        t[i] = thread(test_call_once, i);
+    }
+    
+    for(auto &th:t)
+    {
+        th.join();
+    }
+
+    cout << "g_val:" << g_val << ", count:" << count << endl;
+
+    return 0;
+}
