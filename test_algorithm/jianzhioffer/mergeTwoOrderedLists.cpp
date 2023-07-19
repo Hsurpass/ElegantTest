8949f41d859dacb915e47c42409f30cc40a1637c
hchy <1742143252@qq.com>
2023-07-19 11:21:48 +0800

    add mergeTwoOrderedLists_recursive method

--- test_algorithm/jianzhioffer/mergeTwoOrderedLists.cpp
+++ test_algorithm/jianzhioffer/mergeTwoOrderedLists.cpp
@@ -5,26 +5,23 @@ using namespace std;
 struct LinkList
 {
     int data;
-    struct LinkList *next;
+    struct LinkList* next;
 };
 
-LinkList* mergeTwoOrderedLists(LinkList *l1, LinkList *l2)
+LinkList* mergeTwoOrderedLists(LinkList* l1, LinkList* l2)
 {
     LinkList node;
     LinkList* cur = &node;
 
-    while (l1 && l2)
-    {
-        if(l1->data < l2->data)
-        {
+    while (l1 && l2) {
+        if (l1->data < l2->data) {
             cur->next = l1;
             l1 = l1->next;
-        }   
-        else
-        {
+        }
+        else {
             cur->next = l2;
             l2 = l2->next;
-        } 
+        }
         cur = cur->next;
     }
     cur->next = l1 ? l1 : l2;
@@ -32,21 +29,74 @@ LinkList* mergeTwoOrderedLists(LinkList *l1, LinkList *l2)
     return node.next;
 }
 
-int main()
+void mergeTwoOrderedLists_recursive(LinkList* l1, LinkList* l2, LinkList* p)
+{
+    if (!l1 || !l2) {
+        p->next = l1 ? l1 : l2;
+        return;
+    }
+
+    if (l1->data < l2->data) {
+        p->next = l1;
+        l1 = l1->next;
+    }
+    else {
+        p->next = l2;
+        l2 = l2->next;
+    }
+    p = p->next;
+    mergeTwoOrderedLists_recursive(l1, l2, p);
+}
+
+void test_mergeTwoOrderedLists_recursive()
 {
     LinkList node1{1}, node2{3}, node3{5};
-    LinkList node4{2}, node5{4}, node6{6};
+    LinkList node4{2}, node5{4}, node6{6}, node7{8};
 
-    node1.next = &node2; node2.next = &node3;
-    node4.next = &node5; node5.next = &node6;
+    node1.next = &node2;
+    node2.next = &node3;
+
+    node4.next = &node5;
+    node5.next = &node6;
+    node6.next = &node7;
+
+    LinkList node;
+    LinkList* p = &node;
+    mergeTwoOrderedLists_recursive(&node1, &node4, p);
+
+    p = p->next;
+    while (p) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    putchar(10);
+}
+
+void test_mergeTwoOrderedLists_iterator()
+{
+    LinkList node1{1}, node2{3}, node3{5};
+    LinkList node4{2}, node5{4}, node6{6}, node7{8};
+
+    node1.next = &node2;
+    node2.next = &node3;
+
+    node4.next = &node5;
+    node5.next = &node6;
+    node6.next = &node7;
 
     LinkList* tmp = mergeTwoOrderedLists(&node1, &node4);
-    while(tmp)
-    {
+
+    while (tmp) {
         printf("%d ", tmp->data);
         tmp = tmp->next;
     }
     putchar(10);
-    
+}
+
+int main()
+{
+    test_mergeTwoOrderedLists_recursive();
+    test_mergeTwoOrderedLists_iterator();
+
     return 0;
 }
