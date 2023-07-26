57ada727814bf27fef629428e7a95a8cbe56e0ec
bruce <1742143252@qq.com>
2023-07-26 23:17:56 +0800

    add nth_element test

--- test_cpp/2STL/stl_algorithm/nth_element.cpp
+++ test_cpp/2STL/stl_algorithm/nth_element.cpp
@@ -1,10 +1,11 @@
 #include <algorithm>
+#include <numeric>
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-int main()
+void test_nth_element_basic()
 {
     std::vector<int> nums = {7, 2, 10, 4, 1, 9, 5};
 
@@ -13,10 +14,46 @@ int main()
     // 也就是说第n个位置之前的元素都小于第n个位置上的元素，第n个位置之后的元素都大于第n个位置上的元素，排序的时候n位置上的元素是会发生变化的。
     std::nth_element(nums.begin(), nums.begin() + 2, nums.end());
 
-    for (auto i:nums) { // 1 2 4 7 10 9 5
+    for (auto i : nums) { // 1 2 4 7 10 9 5
         cout << i << " ";
     }
     cout << endl;
-    
+}
+
+void test_nth_element_sort()
+{
+    vector<int> points = {10, 30, 50, 20, 40, 60, 80};
+    vector<int> indices(points.size());
+    std::iota(indices.begin(), indices.end(), 0);
+    for (auto i : indices) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    int mid = ((points.size() - 1) / 2);
+    // std::nth_element(indices.begin(), indices.begin() + mid, indices.end(), [&](int l, int r) {
+    //     return points[l] < points[r];
+    // });
+
+    int* p = indices.data();
+    std::nth_element(p, p + mid, p + points.size(), [&](int l, int r) {
+        return points[l] < points[r];
+    });
+
+    for (auto i : indices) {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (auto i : indices) {
+        cout << points[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // test_nth_element_basic();
+    test_nth_element_sort();
+
     return 0;
 }
