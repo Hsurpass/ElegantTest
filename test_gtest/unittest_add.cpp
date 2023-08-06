c14812b1ddd4037aeecf06bc11365976e77aa306
hchy <1742143252@qq.com>
2023-08-06 17:50:35 +0800

    add test_gtest

---
+++ test_gtest/unittest_add.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "add.h"
+
+class Add_Test : public testing::Test
+{
+public:
+    virtual void SetUp() override
+    {}
+    virtual void TearDown() override
+    {}
+};
+
+TEST_F(Add_Test, positive)
+{
+    int r = add(10, 20);
+    EXPECT_EQ(r, 30);
+}
+
+TEST(Add, positive)
+{
+    int r = add(10, 20);
+    EXPECT_EQ(r, 30);
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
