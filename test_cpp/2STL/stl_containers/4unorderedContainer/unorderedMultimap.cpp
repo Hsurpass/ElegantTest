181134269b588b9cbb4ae2b3395cd1ce21541d8f
hchy <1742143252@qq.com>
2023-08-10 18:41:55 +0800

    update unorderedMultimap.cpp

--- test_cpp/2STL/stl_containers/4unorderedContainer/unorderedMultimap.cpp
+++ test_cpp/2STL/stl_containers/4unorderedContainer/unorderedMultimap.cpp
@@ -25,8 +25,7 @@ void test_unordered_multimap()
 
     cout << endl;
     cout << "mss's buckets contain:\n";
-    for (unsigned i = 0; i < mss.bucket_count(); ++i)
-    {
+    for (unsigned i = 0; i < mss.bucket_count(); ++i) {
         cout << "bucket #" << i << " contains " << mss.bucket_size(i) << ":" << endl;
         for (auto itr = mss.begin(i); itr != mss.end(i); ++itr)
             cout << " " << itr->first << ":" << itr->second << endl;
@@ -51,29 +50,31 @@ void test_erase()
     cout << "bucket count:" << hm.bucket_count() << endl; // 13
 
     // erasing by iterator
-    auto itr = hm.erase(hm.begin());    
+    auto itr = hm.erase(hm.begin());
     cout << itr->first.geta() << " " << itr->second << endl;
     cout << "element num:" << hm.size();                  // 9
     cout << "bucket count:" << hm.bucket_count() << endl; // 13
 
     // erasing by key
-    int eraseNum = hm.erase(6); 
+    int eraseNum = hm.erase(6);
     cout << "eraseNum:" << eraseNum;    // 2
     cout << "element num:" << hm.size();                  // 7
     cout << "bucket count:" << hm.bucket_count() << endl; // 13
 
     // erasing by range
     auto it = hm.erase(hm.find(3), hm.end());
-    if(it == hm.end()) {cout << "it is end" << endl; }
-    else { cout << it->first.geta() << " " << it->second << endl; }
+    if (it == hm.end()) {
+        cout << "it is end" << endl;
+    }
+    else {
+        cout << it->first.geta() << " " << it->second << endl;
+    }
     cout << "element num:" << hm.size();                  // 3
     cout << "bucket count:" << hm.bucket_count() << endl; // 13
 
-    for(auto &x:hm)
-    {
+    for (auto& x : hm) {
         cout << x.first.geta() << " " << x.second << endl;
     }
-
 }
 
 void test_insert()
@@ -125,8 +126,7 @@ void test_equal_range()
     auto r = hm.equal_range(6);
     // auto r = hm.equal_range(8);
     // auto r = hm.equal_range(9);
-    if (r.first != hm.end())
-    {
+    if (r.first != hm.end()) {
         cout << r.first->first.geta() << "," << r.first->second << endl;
     }
     if (r.second != hm.end())       // 左闭右开区间，无序容器末尾不一定指向哪
@@ -134,41 +134,43 @@ void test_equal_range()
         cout << r.second->first.geta() << "," << r.second->second << endl;
     }
 
-    for (auto itr = r.first; itr != r.second; ++itr)
-    {
-        std::cout << itr->first.geta() << " " << itr->second << endl; 
+    for (auto itr = r.first; itr != r.second; ++itr) {
+        std::cout << itr->first.geta() << " " << itr->second << endl;
     }
 
-    for_each(r.first, r.second,
-             [](unordered_multimap<A, int, hash_A>::value_type &x)
-             { std::cout << x.first.geta() << " " << x.second << endl; });
+    for_each(r.first, r.second, [](unordered_multimap<A, int, hash_A>::value_type& x) {
+        std::cout << x.first.geta() << " " << x.second << endl;
+    });
 }
 
 void test_equal_range_with_string()
 {
     typedef std::unordered_multimap<std::string, std::string> stringmap;
-    stringmap myumm = {
-        {"orange", "FL"},
-        {"strawberry", "LA"},
-        {"strawberry", "OK"},
-        {"pumpkin", "NH"}};
+    stringmap myumm = {{"orange", "FL"}, {"strawberry", "LA"}, {"strawberry", "OK"}, {"pumpkin", "NH"}};
 
     // std::cout << "Entries with strawberry:";
     auto range = myumm.equal_range("strawberry");
 
-    if (range.first != myumm.end())
-    {
+    if (range.first != myumm.end()) {
         cout << range.first->first << "," << range.first->second << endl;
     }
-    if (range.second != myumm.end())
-    {
+    if (range.second != myumm.end()) {
         cout << range.second->first << "," << range.second->second << endl;
     }
-    for_each(
-        range.first,
-        range.second,
-        [](stringmap::value_type &x)
-        { std::cout << x.first << " " << x.second; });
+    for_each(range.first, range.second, [](stringmap::value_type& x) {
+        std::cout << x.first << " " << x.second;
+    });
+}
+
+void test_size()
+{
+    std::unordered_multimap<std::string, std::string> myumm = {{"Smith", "Pharmacy"},
+                                                               {"Jones", "Library"},
+                                                               {"Dole", "Church"},
+                                                               {"Smith", "Office"},
+                                                               {"Smith", "Office"}};
+
+    std::cout << "myumm.size() is " << myumm.size() << std::endl;
 }
 
 int main()
@@ -177,7 +179,8 @@ int main()
     // test_insert();
     // test_equal_range();
     // test_equal_range_with_string();
-    test_erase();
+    // test_erase();
+    test_size();
 
     return 0;
 }
