3f1678fffbbf8620b5961b7a1accb2dbb12ce9ea
hchy <1742143252@qq.com>
2023-09-02 17:47:39 +0800

    add test_channel.go

---
+++ test_go/test_channel.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func sum(s []int, c chan int){
+
+}
+
+func test_channel_fifo(){
+	s := []int{1,2,3,4,5,6}
+	for _,v := range s {
+
+	}
+}
+
+func main() {
+	test_channel_fifo()
+}
