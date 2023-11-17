710ad8c61ea262ac53c9f01870d9e8749b54615b
hchy <1742143252@qq.com>
2023-11-17 19:00:39 +0800

    update asio ssl

---
+++ test_boost/asio/examples/c++11/ssl/client.cpp
@@ -0,0 +1,143 @@
+#include <boost/asio/connect.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/context.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/asio/ssl/stream_base.hpp>
+#include <boost/asio/ssl/verify_context.hpp>
+#include <boost/asio/ssl/verify_mode.hpp>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <openssl/x509.h>
+#include <openssl/x509_vfy.h>
+#include <unistd.h>
+
+using boost::asio::ip::tcp;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+enum
+{
+    max_length = 1024
+};
+
+class client
+{
+public:
+    client(boost::asio::io_context& io_context,
+           boost::asio::ssl::context& context,
+           const boost::asio::ip::tcp::resolver::results_type& endpoints)
+        : socket_(io_context, context)
+    {
+        socket_.set_verify_mode(boost::asio::ssl::verify_peer);
+        socket_.set_verify_callback(std::bind(&client::verify_certificate, this, _1, _2));
+        connect(endpoints);
+    }
+
+private:
+    bool verify_certificate(bool preverified, boost::asio::ssl::verify_context& ctx)
+    {
+        // In this example we will simply print the certificate's subject name.
+        char subject_name[256];
+        X509* cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
+        X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
+        std::cout << "Verifying " << subject_name << "\n";
+
+        return preverified;
+    }
+    void connect(const boost::asio::ip::tcp::resolver::results_type& endpoints)
+    {
+        // socket_.lowest_layer().async_connect(const endpoint_type &peer_endpoint)
+        boost::asio::async_connect(socket_.lowest_layer(),
+                                   endpoints,
+                                   [this](const boost::system::error_code& error, const tcp::endpoint& /*endpoint*/) {
+                                       if (!error) {
+                                           handshake();
+                                       }
+                                       else {
+                                           std::cout << "Connect failed: " << error.message() << "\n";
+                                       }
+                                   });
+    }
+    void handshake()
+    {
+        socket_.async_handshake(boost::asio::ssl::stream_base::client, [this](const boost::system::error_code& error) {
+            if (!error) {
+                send_request();
+            }
+            else {
+                std::cout << "Handshake failed: " << error.message() << "\n";
+            }
+        });
+    }
+
+    void send_request()
+    {
+        std::cout << "Enter message: ";
+        std::cin.getline(request_, max_length);
+        size_t request_length = std::strlen(request_);
+
+        boost::asio::async_write(socket_,
+                                 boost::asio::buffer(request_, request_length),
+                                 [this](const boost::system::error_code& error, std::size_t length) {
+                                     if (!error) {
+                                         receive_response(length);
+                                     }
+                                     else {
+                                         std::cout << "Write failed: " << error.message() << "\n";
+                                     }
+                                 });
+    }
+
+    void receive_response(std::size_t length)
+    {
+        boost::asio::async_read(socket_,
+                                boost::asio::buffer(reply_, length),
+                                [this](const boost::system::error_code& error, std::size_t length) {
+                                    if (!error) {
+                                        std::cout << "Reply: ";
+                                        std::cout.write(reply_, length);
+                                        std::cout << "\n";
+                                    }
+                                    else {
+                                        std::cout << "Read failed: " << error.message() << "\n";
+                                    }
+                                });
+    }
+
+    boost::asio::ssl::stream<boost::asio::ip::tcp::socket> socket_;
+    char request_[max_length];
+    char reply_[max_length];
+};
+
+int main(int argc, char* argv[])
+{
+    try {
+        if (argc != 3) {
+            std::cerr << "Usage: client <host> <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_context io_context;
+
+        tcp::resolver resolver(io_context);
+        auto endpoints = resolver.resolve(argv[1], argv[2]);
+
+        boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
+        // ctx.load_verify_file("./mypem/self_ca_sign/ca.crt");
+        // ctx.use_certificate_file("./mypem/self_ca_sign/ca.crt", boost::asio::ssl::context_base::pem);
+        // ctx.use_private_key_file("./mypem/self_ca_sign/ca.key", boost::asio::ssl::context_base::pem);
+        ctx.load_verify_file("/home/hchy/ca.pem");
+        // ctx.load_verify_file("./boostpem/ca.pem");
+
+        client c(io_context, ctx, endpoints);
+        io_context.run();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+}
--- test_boost/asio/examples/c++11/ssl/server.cpp
+++ test_boost/asio/examples/c++11/ssl/server.cpp
@@ -1,5 +1,8 @@
-// g++ server.cpp -L/lib/x86_64-linux-gnu  -lssl -lcrypto -o server
+// g++ server.cpp -lboost_system -L/lib/x86_64-linux-gnu  -lssl -lcrypto -o server
 
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/udp.hpp>
+#include <boost/asio/ssl/stream.hpp>
 #include <iostream>
 #include <exception>
 #include <cstdlib>
@@ -7,8 +10,59 @@
 
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
+#include <memory>
 
 using namespace std;
+
+class session : public std::enable_shared_from_this<session>
+{
+public:
+    session(boost::asio::ssl::stream<boost::asio::ip::tcp::socket> sslsocket /*只能接收一个右值，但是它本身是个左值*/)
+        : sslsocket_(std::move(sslsocket))
+    // session(boost::asio::ssl::stream<boost::asio::ip::tcp::socket>&& sslsocket/*只能接收一个右值，但是它本身是个左值*/) : sslsocket_(std::move(sslsocket))
+    {}
+    void start()
+    {
+        do_handshake();
+    }
+private:
+    void do_handshake()
+    {
+        auto self(shared_from_this());
+        sslsocket_.async_handshake(boost::asio::ssl::stream_base::server,
+                                   [this, self](const boost::system::error_code& error) {
+                                       if (!error) {
+                                           do_read();
+                                       }
+                                   });
+    }
+
+    void do_read()
+    {
+        auto self(shared_from_this());
+        sslsocket_.async_read_some(boost::asio::buffer(data_),
+                                   [this, self](const boost::system::error_code& ec, std::size_t length) {
+                                       if (!ec) {
+                                           do_write(length);
+                                       }
+                                   });
+    }
+
+    void do_write(std::size_t length)
+    {
+        auto self(shared_from_this());
+        boost::asio::async_write(sslsocket_,
+                                 boost::asio::buffer(data_, length),
+                                 [this, self](const boost::system::error_code& ec, std::size_t /*length*/) {
+                                     if (!ec) {
+                                         do_read();
+                                     }
+                                 });
+    }
+    boost::asio::ssl::stream<boost::asio::ip::tcp::socket> sslsocket_;
+    // boost::asio::ssl::stream<boost::asio::ip::udp::socket> udpsocket_;
+    char data_[1024];
+};
 
 class server
 {
@@ -17,18 +71,58 @@ public:
         : acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
           context_(boost::asio::ssl::context::sslv23)
     {
+        // default_workarounds: 这是一个预定义的常量，用于启用针对已知问题的工作绕过方案
+        // no_sslv2: 禁用SSLv2协议
+        // single_dh_use: 指示应该只为每个新的连接创建一个新的临时Diffie-Hellman（DH）参数集，并且这些参数只应用于那个特定的连接。这有助于防止攻击者通过重复使用相同的DH参数来推断密钥材料。
         context_.set_options(boost::asio::ssl::context::default_workarounds | boost::asio::ssl::context::no_sslv2 |
                              boost::asio::ssl::context::single_dh_use);
+
+        // 这是为SSL证书文件设置密码回调函数。当需要访问加密的私钥时，这个回调函数会被调用来获取密码。
         context_.set_password_callback(std::bind(&server::get_password, this));
-        context_.use_certificate_chain_file("server.pem");
-        context_.use_private_key_file("server.pem", boost::asio::ssl::context::pem);
-        context_.use_tmp_dh_file("dh4096.pem");
+        // 这是加载服务器的公钥证书链。这个文件通常包含了服务器的公共证书和所有中间证书。
+        // context_.use_certificate_chain_file("./mypem/self_sign/server.pem");
+        // context_.use_certificate_chain_file("./mypem/self_ca_sign/server.crt");  //*
+        // context_.use_certificate_chain_file("/home/hchy/server.crt");
+
+        context_.use_certificate_chain_file("./boostpem/server.pem");
+
+        // 这是加载服务器的私钥。在这个例子中，私钥也被包含在同一个"sself_sign/erver.pem"文件中，并且使用PEM格式编码。
+        // context_.use_private_key_file("./mypem/self_sign/server.pem", boost::asio::ssl::context::pem);
+        // context_.use_private_key_file("./mypem/self_ca_sign/server.key", boost::asio::ssl::context::pem);
+        context_.use_private_key_file("./boostpem/server.pem", boost::asio::ssl::context::pem);
+
+        // 这是加载Diffie-Hellman参数文件。这些参数用于支持 ephemeral Diffie-Hellman key exchange，这是一种常见的安全协议，可以提供前向安全性。
+        // context_.use_tmp_dh_file("./mypem/self_sign/dh4096.pem");
+        // context_.use_tmp_dh_file("./mypem/self_ca_sign/dh4096.pem");
+        context_.use_tmp_dh_file("./boostpem/dh4096.pem");
+
+        do_accept();
     }
 private:
     std::string get_password() const
     {
+        std::cout << "get passwd" << std::endl;
         return "test";
     }
+
+    void do_accept()
+    {
+        acceptor_.async_accept([this](const boost::system::error_code& error, boost::asio::ip::tcp::socket socket) {
+            if (!error) {
+                // auto sslsocket = boost::asio::ssl::stream<boost::asio::ip::tcp::socket>(std::move(socket), context_);
+                // std::make_shared<session>(std::move(sslsocket));
+                auto&& r_sslsocket = // 左值
+                    boost::asio::ssl::stream<boost::asio::ip::tcp::socket>(std::move(socket), context_);
+                std::make_shared<session>(std::move(r_sslsocket));
+
+                // std::make_shared<session>(
+                //     boost::asio::ssl::stream<boost::asio::ip::tcp::socket>(std::move(socket), context_));
+            }
+
+            do_accept();
+        });
+    }
+
     boost::asio::ip::tcp::acceptor acceptor_;
     boost::asio::ssl::context context_;
 };
