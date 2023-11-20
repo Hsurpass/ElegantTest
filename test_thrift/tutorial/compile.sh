4f4adb1a27cf5a0ff7f93aeefbfb1b4b280213d8
hchy <1742143252@qq.com>
2023-11-20 23:13:13 +0800

    add test_thrift

---
+++ test_thrift/tutorial/compile.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+thrift -r --gen cpp -o ./ tutorial.thrift 
---
+++ test_thrift/tutorial/server.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "gen-cpp/Calculator.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
