1f0e557997f7283db11986e09a6f5d89fd210a4f
hchy <1742143252@qq.com>
2023-11-28 18:13:19 +0800

    update test_matplot

--- test_python/python3/test_matplot/x_square.py
+++ test_python/python3/test_matplot/x_square.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+# sudo apt-get install python3-tk
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+# 查看支持的交互式后端
+#['GTK3Agg', 'GTK3Cairo', 'GTK4Agg', 'GTK4Cairo', 'MacOSX', 
+# 'nbAgg', 'QtAgg', 'QtCairo', 'Qt5Agg', 'Qt5Cairo', 'TkAgg', 
+# 'TkCairo', 'WebAgg', 'WX', 'WXAgg', 'WXCairo']
+print(matplotlib.rcsetup.interactive_bk)    
+
+# 查看支持的非交互式后端
+# ['agg', 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template']
+print(matplotlib.rcsetup.non_interactive_bk)
+
 import matplotlib.pyplot as plt
+
 import numpy as np
 
 x = np.linspace(-10,10,100) # 创建一个包含-10到10之间100个等距点的数组作为x坐标
@@ -11,4 +27,4 @@ plt.xlabel('y') # 设置y轴标签
 plt.title('y = x^2') # 设置图标题
 plt.grid(True) # 显示网格线
 
-plt .show() #显示图形
+plt.show() #显示图形
