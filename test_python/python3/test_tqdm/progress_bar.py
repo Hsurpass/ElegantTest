3d083e3b0645bfd7bf6ccda40e32a5c57bb6398b
hcy <1742143252@qq.com>
2025-10-10 19:35:40 +0800

    tqdm trange

--- test_python/python3/test_tqdm/progress_bar.py
+++ test_python/python3/test_tqdm/progress_bar.py
@@ -2,12 +2,15 @@
 # pip install pyinstaller
 # pyinstaller --onefile progress_bar.py
 
-import tqdm
+from tqdm import tqdm, trange
 import time
 
 iter = range(100)
 
-for i in tqdm.tqdm(iter):
+for i in tqdm(iter, desc="Processing", unit="item"):
 # for i in list(tqdm.tqdm(iter)):
 # for i in tqdm(list(tqdm.tqdm(iter))): # TypeError: 'module' object is not callable
     time.sleep(0.01)
+
+for i in trange(1000):
+    time.sleep(0.001)
